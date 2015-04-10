6d4f081d7435a0c8d4d9ddb08669b742f6be3cdd
Eduardo Poleo <eduardopoleo@gmail.com>
2015-04-10 17:14:53 -0400

    Switch to carrierwave-aws

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,2 @@
-# encoding: utf-8
-
 class AvatarUploader < CarrierWave::Uploader::Base
-  storage :fog
 end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,12 @@
 CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
-    config.storage = :fog
-    config.fog_credentials = {
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV['AWSKEYID'],                        # required
-      :aws_secret_access_key  => ENV['AWSKEYACCESS']                        # required
-    }
-    config.fog_directory  = ENV['BUCKET']                          # required
-  else
-    config.storage = :file
-    config.enable_processing = Rails.env.development?
-  end
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('BUCKET')
+  config.aws_acl    = :public_read
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWSKEYID'),
+    secret_access_key: ENV.fetch('AWSKEYACCESS')
+  }
 end
+
