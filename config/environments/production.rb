324634ae03af1ae50547192b020dd8ce9f8f4159
Eduardo Poleo <eduardopoleo@gmail.com>
2015-04-10 17:39:45 -0400

    Fixed host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => "www.firecamp.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "http://firecamp.herokuapp.com" }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
