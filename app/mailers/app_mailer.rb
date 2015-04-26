858efa7186b51293e57f19d901a802d0c4cd3217
Eduardo Poleo <eduardopoleo@gmail.com>
2015-04-25 21:40:14 -0400

    Fix spelling mistakes

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,7 +1,7 @@
 class AppMailer < ActionMailer::Base
   def welcome_notification(user)
     @user = user
-    mail from: 'info@firecamp.ca', to: user.email, subject: "You created a new todo"
+    mail from: 'info@firecamp.ca', to: user.email, subject: "Welcome to Firecamp"
   end
 
   def send_forgot_password(user)
